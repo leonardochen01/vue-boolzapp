c3f7b09e5b9e413df0bb7fff177e787fe9bdd9c2
leonardochen01 <leonardochen.lc@gmail.com>
2023-04-17 14:46:06 +0200

    MILESTONE 1: added left sidebar

--- MILESTONE 1/js/script.js
+++ MILESTONE 1/js/script.js
@@ -4,6 +4,42 @@ createApp({
   data(){
     return{
 
+      //array di persone
+      contacts:[
+        {
+          user: 'Michele',
+          profile: 'img/avatar_1.jpg'
+        },
+        {
+          user: 'Fabio',
+          profile: 'img/avatar_2.jpg'
+        },
+        {
+          user: 'Samuele',
+          profile: 'img/avatar_3.jpg'
+        },
+        {
+          user: 'Alessandro B.',
+          profile: 'img/avatar_4.jpg'
+        },
+        {
+          user: 'Alessandro L.',
+          profile: 'img/avatar_5.jpg'
+        },
+        {
+          user: 'Claudia',
+          profile: 'img/avatar_6.jpg'
+        },
+        {
+          user: 'Federico',
+          profile: 'img/avatar_7.jpg'
+        },
+        {
+          user: 'Davide',
+          profile: 'img/avatar_8.jpg'
+        }
+      ]
+
     }
   }
 }).mount('#app')
